5c9f1a99d3d05f63cb083786271063cc81753082
neoxolotl <fernanditososa@gmail.com>
2020-09-24 18:37:59 -0300

    instance

--- instance/instance.tf
+++ instance/instance.tf
@@ -1,17 +1,59 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
+resource "google_compute_instance_template" "default" {
+  name        = "appserver-template"
+  description = "This template is used to create app server instances."
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
+  tags = ["foo", "bar"]
+
+  labels = {
+    environment = "dev"
+  }
+
+  instance_description = "description assigned to instances"
+  machine_type         = "n1-standard-1"
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  // Create a new boot disk from an image
+  disk {
+    source_image = "debian-cloud/debian-9"
+    auto_delete  = true
+    boot         = true
+  }
+
+  // Use an existing disk resource
+  disk {
+    // Instance Templates reference disks by name, not self link
+    source      = google_compute_disk.foobar.name
+    auto_delete = false
+    boot        = false
   }
 
   network_interface {
-    # A default network is created for all GCP projects
     network = "default"
-    access_config {
-    }
   }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+data "google_compute_image" "my_image" {
+  family  = "debian-9"
+  project = "debian-cloud"
+}
+
+resource "google_compute_disk" "foobar" {
+  name  = "existing-disk"
+  image = data.google_compute_image.my_image.self_link
+  size  = 10
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
 }
