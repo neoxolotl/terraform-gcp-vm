7960fa0832965a806ede77d81ea1ee4096ced4a4
neoxolotl <fernanditososa@gmail.com>
2020-09-24 19:04:37 -0300

    i

--- instance/instance.tf
+++ instance/instance.tf
@@ -1,28 +1,28 @@
-data "google_compute_image" "debian" {
-  family  = "ubuntu-1804-lts"
-  project = "gce-uefi-images"
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 8
 }
 
-# Creates a GCP VM Instance.
-resource "google_compute_instance" "vm" {
-  name         = var.name
-  machine_type = var.machine_type
-  zone         = var.zone
-  tags         = ["http-server"]
-  labels       = var.labels
+// A single Compute Engine instance
+resource "google_compute_instance" "default" {
+ name         = "flask-vm-${random_id.instance_id.hex}"
+ machine_type = "f1-micro"
+ zone         = "us-west1-a"
 
-  boot_disk {
-    initialize_params {
-      image = data.google_compute_image.debian.self_link
-    }
-  }
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
 
-  network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral IP
-    }
-  }
+// Make sure flask is installed on all new instances for later steps
+ metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
 
-  metadata_startup_script = data.template_file.nginx.rendered
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
 }
